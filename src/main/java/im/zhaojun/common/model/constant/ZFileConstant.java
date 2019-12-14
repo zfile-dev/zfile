2aa9ccc3897b02395fbb9ecee95a0311835cc6c8
zhaojun1998 <root@zhaojun.im>
2019-12-14 22:01:04 +0800

    :art: 修改文件头, 文件尾, 密码文件的名称为可配置

--- src/main/java/im/zhaojun/common/model/constant/ZFileConstant.java
+++ src/main/java/im/zhaojun/common/model/constant/ZFileConstant.java
@@ -1,5 +1,10 @@
 package im.zhaojun.common.model.constant;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ZFileConstant {
 
     public final static String USER_HOME = System.getProperty("user.home");
@@ -9,16 +14,31 @@ public class ZFileConstant {
     /**
      * 页面头部文件
      */
-    public static final String HEADER_FILE_NAME = "header.md";
+    public static String HEADER_FILE_NAME = "header.md";
 
     /**
      * 页面尾部文件
      */
-    public static final String FOOTER_FILE_NAME = "footer.md";
+    public static String FOOTER_FILE_NAME = "footer.md";
 
     /**
      * 密码文件
      */
-    public static final String PASSWORD_FILE_NAME = "password.txt";
+    public static String PASSWORD_FILE_NAME = "password.txt";
+
+    @Autowired(required = false)
+    public void setHeaderFileName(@Value("${zfile.constant.header}") String headerFileName) {
+        ZFileConstant.HEADER_FILE_NAME = headerFileName;
+    }
+
+    @Autowired(required = false)
+    public void setFooterFileName(@Value("${zfile.constant.footer}") String footerFileName) {
+        ZFileConstant.FOOTER_FILE_NAME = footerFileName;
+    }
+
+    @Autowired(required = false)
+    public void setPasswordFileName(@Value("${zfile.constant.password}") String passwordFileName) {
+        ZFileConstant.PASSWORD_FILE_NAME = passwordFileName;
+    }
 
 }
