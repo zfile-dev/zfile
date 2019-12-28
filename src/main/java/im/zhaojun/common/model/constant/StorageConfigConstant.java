9aaf1494b1aabc3ac00949eec6a43ca9ed38c3f3
zhaojun1998 <root@zhaojun.im>
2019-12-28 23:15:34 +0800

    :art: 抽取通用变量

---
+++ src/main/java/im/zhaojun/common/model/constant/StorageConfigConstant.java
@@ -0,0 +1,33 @@
+package im.zhaojun.common.model.constant;
+
+/**
+ * @author zhaojun
+ * @date 2019/12/28 18:47
+ */
+public class StorageConfigConstant {
+
+    public static final String BUCKET_NAME_KEY = "bucket-name";
+
+    public static final String SECRET_ID_KEY = "secretId";
+
+    public static final String ACCESS_KEY = "accessKey";
+
+    public static final String SECRET_KEY = "secretKey";
+
+    public static final String ENDPOINT_KEY = "endPoint";
+
+    public static final String BASE_PATH = "base-path";
+
+    public static final String DOMAIN_KEY = "domain";
+
+    public static final String USERNAME_KEY = "username";
+
+    public static final String PASSWORD_KEY = "password";
+
+    public static final String HOST_KEY = "host";
+
+    public static final String PORT_KEY = "port";
+
+    public static final String FILE_PATH_KEY = "filePath";
+
+}
