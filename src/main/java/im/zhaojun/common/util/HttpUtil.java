f1288820348c10b11ded9cef2c31e39d2acbaceb
zhaojun1998 <root@zhaojun.im>
2020-01-05 15:54:59 +0800

    :sparkles: 添加文件获取和判断是否存在接口

--- src/main/java/im/zhaojun/common/util/HttpUtil.java
+++ src/main/java/im/zhaojun/common/util/HttpUtil.java
@@ -1,5 +1,6 @@
 package im.zhaojun.common.util;
 
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -11,6 +12,17 @@ public class HttpUtil {
         RestTemplate restTemplate = SpringContextHolder.getBean(RestTemplate.class);
         String result = restTemplate.getForObject(url, String.class);
         return result == null ? "" : result;
+    }
+
+    public static boolean checkUrlExist(String url) {
+        RestTemplate restTemplate = SpringContextHolder.getBean(RestTemplate.class);
+        try {
+            restTemplate.headForHeaders(url);
+            return true;
+        } catch (RestClientException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
 }
