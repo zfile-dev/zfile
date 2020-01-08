47e88849ac34cd3447a71f9418aab4bc3c39b9f4
zhaojun1998 <root@zhaojun.im>
2020-01-08 22:32:47 +0800

    :bug: 修复当前存储引擎为空时, 与新设置的存储引擎比较出现的 NPE

--- src/main/java/im/zhaojun/common/controller/AdminController.java
+++ src/main/java/im/zhaojun/common/controller/AdminController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -58,7 +59,7 @@ public class AdminController {
         systemConfigDTO.setId(1);
         systemConfigService.updateSystemConfig(systemConfigDTO);
 
-        if (!currentStorageStrategy.equals(systemConfigDTO.getStorageStrategy())) {
+        if (!Objects.equals(currentStorageStrategy, systemConfigDTO.getStorageStrategy())) {
             refreshStorageStrategy();
         }
 
