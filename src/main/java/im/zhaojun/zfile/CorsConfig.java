a7ee9473666c0333526dd5ff621d17d60ced7e19
zcxzzz <34907358+zcxzzz@users.noreply.github.com>
2020-06-17 14:52:01 +0800

    跨域
    解决跨域问题

---
+++ src/main/java/im/zhaojun/zfile/CorsConfig.java
@@ -0,0 +1,18 @@
+package im.zhaojun.zfile;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); //允许任何域名
+        corsConfiguration.addAllowedHeader("*"); //允许任何头
+        corsConfiguration.addAllowedMethod("*"); //允许任何方法
+        return corsConfiguration;
+    }
+
+}
