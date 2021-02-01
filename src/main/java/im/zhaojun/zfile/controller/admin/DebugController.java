3b6e2be7fe044d032e4d00ace4d7b0768f38fe38
zhaojun1998 <root@zhaojun.im>
2021-02-01 23:14:29 +0800

    :sparkles: 新增 debug 模式, 可访问数据库控制台和重置密码

---
+++ src/main/java/im/zhaojun/zfile/controller/admin/DebugController.java
@@ -0,0 +1,31 @@
+package im.zhaojun.zfile.controller.admin;
+
+
+import im.zhaojun.zfile.model.support.ResultBean;
+import im.zhaojun.zfile.service.SystemConfigService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.Resource;
+
+@Controller
+public class DebugController {
+
+    @Value("${zfile.debug}")
+    private Boolean debug;
+
+    @Resource
+    private SystemConfigService systemConfigService;
+
+    @GetMapping("/debug/resetPwd")
+    public ResultBean resetPwd() {
+        if (debug) {
+            systemConfigService.updateUsernameAndPwd("admin", "123456");
+            return ResultBean.success();
+        } else {
+            return ResultBean.error("未开启 DEBUG 模式，不允许进行此操作。");
+        }
+    }
+
+}
