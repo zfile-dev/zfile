3f8beb2f0bc4940bcecc62cf2c64eb973a274a43
zhaojun1998 <root@zhaojun.im>
2021-02-24 18:40:10 +0800

    :sparkles: debug 模式优化, 增加到系统参数接口中

--- src/main/java/im/zhaojun/zfile/controller/home/FileController.java
+++ src/main/java/im/zhaojun/zfile/controller/home/FileController.java
@@ -20,6 +20,7 @@ import im.zhaojun.zfile.util.FileComparator;
 import im.zhaojun.zfile.util.HttpUtil;
 import im.zhaojun.zfile.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
