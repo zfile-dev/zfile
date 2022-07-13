e1df2f886b943afa775f65de30ba1ac869f52272
zhaojun <873019219@qq.com>
2022-07-13 20:18:48 +0800

    :bug: 修复 url 路径中带 . 访问 404 的 bug

--- src/main/java/im/zhaojun/zfile/common/config/WebMvcConfig.java
+++ src/main/java/im/zhaojun/zfile/common/config/WebMvcConfig.java
@@ -1,11 +1,18 @@
 package im.zhaojun.zfile.common.config;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * ZFile Web 相关配置.
@@ -39,6 +46,19 @@ public class WebMvcConfig implements WebMvcConfigurer {
             connector.setProperty("relaxedQueryChars", "<>[\\]^`{|}%[]");
         });
         return webServerFactory;
+    }
+
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer(){
+        return factory -> {
+            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/index.html");
+            ErrorPage error200Page = new ErrorPage(HttpStatus.OK, "/index.html");
+            Set<ErrorPage> errorPages = new HashSet<>();
+            errorPages.add(error404Page);
+            errorPages.add(error200Page);
+            factory.setErrorPages(errorPages);
+        };
     }
 
 }
