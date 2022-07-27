b2fb1af99b9a360a5c35e009550c34bd48efb682
zhaojun <873019219@qq.com>
2022-07-27 20:49:39 +0800

    :memo: 修复本地存储文件路径描述，对于 docker 环境更容易理解。

--- src/main/java/im/zhaojun/zfile/admin/model/param/LocalParam.java
+++ src/main/java/im/zhaojun/zfile/admin/model/param/LocalParam.java
@@ -11,7 +11,7 @@ import lombok.Getter;
 @Getter
 public class LocalParam extends ProxyDownloadParam {
 
-	@StorageParamItem(name = "文件路径", description = "只支持绝对路径<br>Docker 部署需提前映射宿主机路径！ " +
+	@StorageParamItem(name = "文件路径", description = "只支持绝对路径<br>Docker 方式部署的话需提前映射宿主机路径！ " +
 			"(<a class='link' target='_blank' href='https://docs.docker.com/engine/reference/run/#volume-shared-filesystems'>配置文档</a>)")
 	private String filePath;
 
