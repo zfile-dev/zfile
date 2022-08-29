a2fa8b3eeb1112961cf632f87da89d2f6f15f989
zhaojun <873019219@qq.com>
2022-08-29 14:38:47 +0800

    :bug: 修复没有自动创建数据库目录的 bug

--- src/main/java/im/zhaojun/zfile/ZfileApplication.java
+++ src/main/java/im/zhaojun/zfile/ZfileApplication.java
@@ -1,15 +1,10 @@
 package im.zhaojun.zfile;
 
-import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.util.StrUtil;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(exposeProxy = true, proxyTargetClass = true)
@@ -22,22 +17,5 @@ public class ZfileApplication {
         SpringApplication.run(ZfileApplication.class, args);
     }
 
-    @Value("${spring.datasource.driver-class-name}")
-    private String datasourceDriveClassName;
-
-    @Value("${spring.datasource.url}")
-    private String datasourceUrl;
-
-
-    @PostConstruct
-    public void init() {
-        if (StrUtil.equals(datasourceDriveClassName, "org.sqlite.JDBC")) {
-            String path = datasourceUrl.replace("jdbc:sqlite:", "");
-            String folderPath = FileUtil.getParent(path, 1);
-            if (!FileUtil.exist(folderPath)) {
-                FileUtil.mkdir(folderPath);
-            }
-        }
-    }
 
 }
--- src/main/java/im/zhaojun/zfile/common/config/MyBatisPlusConfig.java
+++ src/main/java/im/zhaojun/zfile/common/config/MyBatisPlusConfig.java
@@ -1,11 +1,15 @@
 package im.zhaojun.zfile.common.config;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.sql.SQLException;
@@ -20,8 +24,27 @@ public class MyBatisPlusConfig {
 
     @Resource
     private DataSource dataSource;
-
-
+    
+    @Value("${spring.datasource.driver-class-name}")
+    private String datasourceDriveClassName;
+    
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+    
+    /**
+     * 如果是 sqlite 数据库，自动创建数据库文件所在目录
+     */
+    @PostConstruct
+    public void init() {
+        if (StrUtil.equals(datasourceDriveClassName, "org.sqlite.JDBC")) {
+            String path = datasourceUrl.replace("jdbc:sqlite:", "");
+            String folderPath = FileUtil.getParent(path, 1);
+            if (!FileUtil.exist(folderPath)) {
+                FileUtil.mkdir(folderPath);
+            }
+        }
+    }
+    
     /**
      * mybatis plus 分页插件配置
      */
