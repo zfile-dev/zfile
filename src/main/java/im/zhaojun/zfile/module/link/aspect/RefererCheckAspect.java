dcdd25c01ff5268da4058582ba17144a8e5ec6c5
zhaojun <873019219@qq.com>
2022-11-26 17:55:15 +0800

    :bug: 修复 referer 防盗链允许为空时，仍然去黑/白名单校验的 bug

--- src/main/java/im/zhaojun/zfile/module/link/aspect/RefererCheckAspect.java
+++ src/main/java/im/zhaojun/zfile/module/link/aspect/RefererCheckAspect.java
@@ -86,6 +86,8 @@ public class RefererCheckAspect {
 			log.warn("请求路径 {}, referer 为白名单模式，当前请求 referer {} 未在白名单中，禁止访问.", requestUrl, referer);
 			httpServletResponse.sendRedirect(forbiddenUrl);
 			return null;
+		} else if (refererAllowEmpty && StrUtil.isEmpty(referer)) { // 如果 referer 允许为空，且当前 referer 为空，则跳过校验
+			return point.proceed();
 		}
 
 		// 如果是黑名单模式，则校验当前 referer 是否在列表中，则禁止访问.
