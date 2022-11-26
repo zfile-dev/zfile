5bfa7037cbc08ca38ae278fb229f334eeea0a732
zhaojun <873019219@qq.com>
2022-11-26 18:10:26 +0800

    :pencil2: 增加 SharePoint 网站提示文本

--- src/main/java/im/zhaojun/zfile/module/storage/model/param/SharePointParam.java
+++ src/main/java/im/zhaojun/zfile/module/storage/model/param/SharePointParam.java
@@ -17,7 +17,7 @@ public class SharePointParam extends MicrosoftDriveParam {
 	@StorageParamItem(name = "SecretKey", defaultValue = "${zfile.onedrive.clientSecret}", order = 2)
 	private String clientSecret;
 
-	@StorageParamItem(name = "网站", order = 5)
+	@StorageParamItem(name = "网站", order = 5, description = "如此处选择不到网站，请检查世纪互联中网站隐私设置是否为\"公用-组织中的任何人都可访问此站点\"")
 	private String siteId;
 
 	@StorageParamItem(name = "子目录", order = 6, description = "表示 SharePoint 子列表/子网站，在世纪互联网站 Tab 卡中 \"网站内容\" 新增.")
