b95c1d890b0df4f28491a63efc49dbc0eee27206
zhaojun <873019219@qq.com>
2023-03-05 15:29:48 +0800

    :sparkles: 更显眼的提示用户腾讯云使用 CDN 回源鉴权后需要关闭 ZFile 中私有空间开关。

--- src/main/java/im/zhaojun/zfile/module/storage/model/param/TencentParam.java
+++ src/main/java/im/zhaojun/zfile/module/storage/model/param/TencentParam.java
@@ -1,6 +1,7 @@
 package im.zhaojun.zfile.module.storage.model.param;
 
 import im.zhaojun.zfile.module.storage.annotation.StorageParamItem;
+import im.zhaojun.zfile.module.storage.model.enums.StorageParamTypeEnum;
 import lombok.Getter;
 
 /**
@@ -13,5 +14,8 @@ public class TencentParam extends S3BaseParam {
 
 	@StorageParamItem(key = "secretId", name = "SecretId", order = 1)
 	private String accessKey;
+
+	@StorageParamItem(name = "是否是私有空间", order = 7, type = StorageParamTypeEnum.SWITCH, defaultValue = "true", description = "私有空间会生成带签名的下载链接. <font color=\"red\">如您使用自定义CDN域名，且在腾讯云开启了回源鉴权，请务必关闭此选项。</font>")
+	private boolean isPrivate;
 
 }
