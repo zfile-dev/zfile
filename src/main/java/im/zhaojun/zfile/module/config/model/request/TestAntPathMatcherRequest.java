b86e487a47690abfec8dbc2f96d7be7676ed0291
zhaojun <873019219@qq.com>
2023-05-27 16:51:09 +0800

    :sparkles: 新增辅助测试 ant 表达式请求参数

---
+++ src/main/java/im/zhaojun/zfile/module/config/model/request/TestAntPathMatcherRequest.java
@@ -0,0 +1,12 @@
+package im.zhaojun.zfile.module.config.model.request;
+
+import lombok.Data;
+
+@Data
+public class TestAntPathMatcherRequest {
+
+    private String antPath;
+
+    private String testPath;
+
+}
