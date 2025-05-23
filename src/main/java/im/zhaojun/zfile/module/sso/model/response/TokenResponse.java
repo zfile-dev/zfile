c4c770907e90a5c65a612497cd6b1e0f89ee10f0
zhaojun <873019219@qq.com>
2025-05-23 09:27:26 +0800

    指定 fastjson 转换对象时支持下划线转驼峰

--- src/main/java/im/zhaojun/zfile/module/sso/model/response/TokenResponse.java
+++ src/main/java/im/zhaojun/zfile/module/sso/model/response/TokenResponse.java
@@ -1,11 +1,8 @@
 package im.zhaojun.zfile.module.sso.model.response;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 @Data
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class TokenResponse {
 
     private String idToken;
