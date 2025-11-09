2049308b51eab0c0f35b8ab642c4849a34c793c1
zhaojun <873019219@qq.com>
2025-11-09 14:02:30 +0800

    修复分享文件夹启用权限校验 bug

--- src/main/java/im/zhaojun/zfile/core/util/ZFileAuthUtil.java
+++ src/main/java/im/zhaojun/zfile/core/util/ZFileAuthUtil.java
@@ -2,6 +2,7 @@ package im.zhaojun.zfile.core.util;
 
 import cn.dev33.satoken.stp.StpUtil;
 import cn.hutool.extra.spring.SpringUtil;
+import im.zhaojun.zfile.module.share.context.ShareAccessContext;
 import im.zhaojun.zfile.module.user.model.constant.UserConstant;
 import im.zhaojun.zfile.module.user.model.entity.User;
 import im.zhaojun.zfile.module.user.service.UserService;
@@ -20,7 +21,13 @@ public class ZFileAuthUtil {
 			userService = SpringUtil.getBean(UserService.class);
 		}
 
-		return userService.getById(StpUtil.getLoginId(UserConstant.ANONYMOUS_ID));
+        // 检查是否为分享访问，如果是则返回分享者用户 ID
+        if (ShareAccessContext.isShareAccess()) {
+            Integer shareUserId = ShareAccessContext.getShareUserId();
+            return userService.getById(shareUserId);
+        }
+
+        return userService.getById(StpUtil.getLoginId(UserConstant.ANONYMOUS_ID));
 	}
 
 	public static Integer getCurrentUserId() {
@@ -28,7 +35,12 @@ public class ZFileAuthUtil {
 			userService = SpringUtil.getBean(UserService.class);
 		}
 
-		try {
+        // 检查是否为分享访问，如果是则返回分享者用户 ID
+        if (ShareAccessContext.isShareAccess()) {
+            return ShareAccessContext.getShareUserId();
+        }
+
+        try {
 			return StpUtil.getLoginId(UserConstant.ANONYMOUS_ID);
 		} catch (Exception e) {
 			return UserConstant.ANONYMOUS_ID;
